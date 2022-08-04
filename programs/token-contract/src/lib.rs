f386cd319a41c0acf4bf2f3148c66b16ef4ab8c7
qasimInvoZone <qasim.rehman@invozone.com>
2022-08-04 16:44:48 +0500

    first commit

---
+++ programs/token-contract/src/lib.rs
@@ -0,0 +1,71 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Token, MintTo, Transfer};
+
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod token_contract {
+    use super::*;
+
+    pub fn mint_token(ctx: Context<MintToken>,) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        anchor_spl::token::mint_to(cpi_ctx,10)?;
+        Ok(())
+    }
+
+    pub fn transfer_token(ctx: Context<TransferToken>,) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        anchor_spl::token::transfer(cpi_ctx, 5)?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_program: Program<'info, Token>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub from: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+}
